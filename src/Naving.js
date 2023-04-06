fd3e78128cb347322018fabf46ac1146b7d5fb69
Sasirekha03 <108916484+Sasirekha03@users.noreply.github.com>
2023-04-06 07:08:38 +0530

    Update Naving.js

--- src/Naving.js
+++ src/Naving.js
@@ -6,22 +6,10 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from './logo.svg'
 
-//import { Router, Switch, Route } from "react-router-dom";
-//import { BrowserRouter as Router } from 'react-router-dom';
 
-//import Vdart from './Vdart.png';
-
-
-//import Nav from 'react-bootstrap/Nav';
 function Naving() {
   return (
-    
-    
-
     <div>
-    
-      {/*<img src={Vdart}alt="Company Logo" className="logo" ></img>*/}
-
       <Navbar bg="dark" variant="dark" logo={logo}>
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
